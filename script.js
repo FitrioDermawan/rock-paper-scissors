f082182426565bd1a1f605634a48f0de493cab3f
Fitrio Dermawan <riodermawan1912@gmail.com>
2024-04-10 20:42:16 +0700

    Make choice input for the player.

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ function getComputerChoice() {
     let randomNum = Math.round(Math.random()*100);
 
     if (randomNum > 32 && randomNum < 66) {
-        return "Rock";
+        return "rock";
     } else if (randomNum < 33) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
@@ -16,14 +16,18 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return "It's a tie!"
-    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
         return "You Lose! Paper beats Rock"
     } else {
-        return "You Win! Scissors beats Rock"
+        return "You Win! Rock beats Scissors"
     }
 }
 
-const playerSelection = "Rock";
+// Player input the choice and make it case-insensitive
+let selection = prompt("Rock, Paper, Scissors?", "");
+const playerSelection = selection.toLowerCase();
+
 const computerSelection = getComputerChoice();
 
 console.log(playRound(playerSelection, computerSelection))
+
