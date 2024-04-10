49fad0f42b3372de99ef7342339c7a3262fe083a
Fitrio Dermawan <riodermawan1912@gmail.com>
2024-04-10 20:19:22 +0700

    - Add descriptive comments. - Change variable declared to return in function getComputerChoice. - Add function playRound that compare the selection between player and computer and show the round result.

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-function getComputerChoice() {
-    let choice;
-    
-    let randomNum = Math.round(Math.random()*100)+1;
-    console.log(randomNum);
+// Getting random choice for computer
+function getComputerChoice() { 
+    let randomNum = Math.round(Math.random()*100);
 
-    if (randomNum > 30 && randomNum < 60) {
-        choice = "Rock";
-    } else if (randomNum < 30) {
-        choice = "Paper";
+    if (randomNum > 32 && randomNum < 66) {
+        return "Rock";
+    } else if (randomNum < 33) {
+        return "Paper";
     } else {
-        choice = "Scissors";
+        return "Scissors";
     }
-
-    console.log(choice);
 }
 
-getComputerChoice();
+
+// Plays a single round and show result
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie!"
+    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        return "You Lose! Paper beats Rock"
+    } else {
+        return "You Win! Scissors beats Rock"
+    }
+}
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection))
